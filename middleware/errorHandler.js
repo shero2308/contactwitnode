7b76684a8425b95a5082d74fb882ab40ec7090d2
shero <rakshitkumar682@gmail.com>
2025-03-29 16:42:57 +0530

    Implement error handling middleware and update server configuration

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-    
+    const statuscode = res.statuscode ? res.statuscode : 500;
+    res.json({message: err.message , stackTrace: err.stack})
 };
 
 module.exports = errorHandler;
