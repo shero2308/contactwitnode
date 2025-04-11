91e11404d1f0c05537048ab4e223c6af32bc45e2
shero <rakshitkumar682@gmail.com>
2025-04-11 15:56:27 +0530

    Implement JWT authentication in login functionality and add jsonwebtoken dependency

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/UserModels');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //@desc Register user
 //@route POST /api/users/register
@@ -51,7 +52,27 @@ module.exports = { RegisterUser };
 //@access Public
 
 const LoginUser = asyncHandler(async(req, res) => {
-    res.json({message: "login User"})
+    const { email, password } = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("Please include all fields")
+    }
+    const user = await User.findOne({email});
+    if(user &&(await bcrypt.compare(password , user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        },process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1m' }
+        );
+        res.status(200).json({accessToken})
+    } else {
+        res.status(401);
+        throw new Error("Invalid credentials")
+    }
 });
 
 //@desc Current user
